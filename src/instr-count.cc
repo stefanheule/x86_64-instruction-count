7dfdc6dc5fbb149ecd5efb864b86e535b30670c7
Stefan Heule <stefanheule@gmail.com>
2015-08-21 20:02:15 -0700

    finish all modes

--- src/instr-count.cc
+++ src/instr-count.cc
@@ -22,15 +22,18 @@ auto& mode = ValueArg<Mode, ModeReader, ModeWriter>::create("mode")
 auto& show_instructions = FlagArg::create("show_instructions")
   .description("Should the instructions be shown (rather than just showing the count)?");
 
+uint16_t type_size(Type t);
+
 /** Count the number of x86_x64 instructions in different ways. */
 int main(int argc, char** argv) {
 
   CommandLineConfig::strict_with_convenience(argc, argv);
 
   map<string, set<Opcode>> instructions;
-  for(size_t i = 0; i < X64ASM_NUM_OPCODES; ++i) {
+  for(size_t i = 1; i < X64ASM_NUM_OPCODES; ++i) {
     auto opcode = (Opcode)i;
-    //auto instr = Instruction(opcode);
+    auto instr = Instruction(opcode);
+    stringstream ss;
     string key;
 
     if (mode == Mode::MNEMONIC_ATT) {
@@ -38,9 +41,18 @@ int main(int argc, char** argv) {
     } else if (mode == Mode::MNEMONIC_INTEL) {
       key = opcode_write_intel(opcode);
     } else if (mode == Mode::OPERAND_TYPE) {
-      
+      ss << opcode;
+      key = ss.str();
+      // don't count the _1 variants
+      if (key.compare(key.length() - 2, 2, "_1") == 0 || key.compare(key.length() - 2, 2, "_2") == 0 || key.compare(key.length() - 2, 2, "_3") == 0) {
+        key = key.substr(key.length() - 2);
+      }
     } else if (mode == Mode::OPERAND_WIDTH) {
-      
+      ss << opcode_write_intel(opcode);
+      for (size_t j = 0; j < instr.arity(); j++) {
+        ss << "_" << type_size(instr.type(j));
+      }
+      key = ss.str();
     }
 
     if (instructions.find(key) == instructions.end()) {
@@ -63,4 +75,55 @@ int main(int argc, char** argv) {
   }
 
   return 0;
+}
+
+uint16_t type_size(Type t) {
+
+  switch(t) {
+  case Type::IMM_8:
+  case Type::ZERO:
+  case Type::ONE:
+  case Type::THREE:
+  case Type::M_8:
+  case Type::MOFFS_8:
+  case Type::RH:
+  case Type::R_8:
+  case Type::AL:
+  case Type::CL:
+    return 8;
+
+  case Type::IMM_16:
+  case Type::M_16:
+  case Type::R_16:
+  case Type::MOFFS_16:
+  case Type::AX:
+  case Type::DX:
+    return 16;
+
+  case Type::IMM_32:
+  case Type::MOFFS_32:
+  case Type::M_32:
+  case Type::R_32:
+  case Type::EAX:
+    return 32;
+
+  case Type::IMM_64:
+  case Type::MOFFS_64:
+  case Type::M_64:
+  case Type::R_64:
+  case Type::RAX:
+    return 64;
+
+  case Type::XMM:
+  case Type::XMM_0:
+  case Type::M_128:
+    return 128;
+
+  case Type::YMM:
+  case Type::M_256:
+    return 256;
+
+  default:
+    return 0;
+  }
 }
